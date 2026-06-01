b12e9204b2a8cf394b7dddceb4771687f0dcfac3
mayur-412 <mayurzezariya990@gmail.com>
2026-06-01 05:20:19 -0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,26 @@
+var acc = document.getElementsByClassName("accordion-title");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+  document.addEventListener(
+                "DOMContentLoaded", () => {
+                    new Mmenu( "#menu", {
+                       "slidingSubmenus": false,
+                       "offCanvas": {
+                          "position": "right-front"
+                       },
+                       "theme": "light"
+                    });
+                }
+            );
